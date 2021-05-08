d3bfb899bce96859df3a3b80171293d0f6a5ad17
paul <paul_beavers@mac.com>
2021-05-08 08:29:07 -0500

    initial

---
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,23 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log('paul');
+console.log(43);
+let country = "USA";
+let continent = "North America";
+let population = 3000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+
+let firstName = "matlda";
+let first = "paul";
+
+let myFirstJob = "programmer";
+
+console.log(firstName);
+console.log(firstName);
+console.log(myFirstJob);
